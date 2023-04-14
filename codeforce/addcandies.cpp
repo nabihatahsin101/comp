62a13b84816a74e0c95e880be8cd42ba4fc3ec21
nabihatahsin101 <124432334+nabihatahsin101@users.noreply.github.com>
2023-04-15 00:37:54 +0600

    Create addcandies.cpp

---
+++ codeforce/addcandies.cpp
@@ -0,0 +1,18 @@
+https://codeforces.com/contest/1447
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t,n,i;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        cout<<n<<endl;
+        for(i=1;i<=n;i++)
+        {
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+}
